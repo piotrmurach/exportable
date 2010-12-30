d4d3fc35d5088e1bf4bbcdf9f958a24707a71e72
Piotr Murach <p_murek@wp.pl>
2010-12-30 16:26:56 +0000

    Boilerplate code for mixins.

--- lib/exportable.rb
+++ lib/exportable.rb
@@ -1,4 +1,24 @@
 # Exportable
 module Exportable
+  
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  module ClassMethods
+    
+    def acts_as_exportable(options = {}, &extension)
+      return if self.included_modules.include? Exportable
+
+      send :include, Exportable
+
+      extend Exportable::SingletonMethods
+    end
+
+  end
+
+  module SingletonMethods
+    
+  end
 
 end
